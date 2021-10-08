4f012e8f3ffe89b49766656aa996d902c551afea
Clarathms <80729748+Clarathms@users.noreply.github.com>
2021-10-08 12:02:03 -0300

    Add files via upload

---
+++ Soma_vetores.py
@@ -0,0 +1,24 @@
+#Faça um programa para ler dois vetores A e B, contendo N elementos cada. Em seguida, gere um
+#terceiro vetor C onde cada elemento de C é a soma dos elementos correspondentes de A e B. Imprima
+#o vetor C gerado.
+
+N = int(input("Quantos valores vai ter cada vetor? "))
+vetA: [int] = [0 for x in range (N)]
+vetB: [int] = [0 for y in range (N)]
+vetC: [int] = [0 for z in range (N)]
+
+
+print("Digite os valores do vetor A:")
+for i in range (0,N):
+    vetA[i] = int(input())
+
+print("Digite os valores do vetor B:")
+for i in range (0,N):
+    vetB[i] = int(input())
+
+print("VETOR RESULTANTE:")
+for i in range(0, N):
+    vetC[i] = vetA[i] + vetB[i]
+
+for k in range(0,N):
+    print(vetC[k])
